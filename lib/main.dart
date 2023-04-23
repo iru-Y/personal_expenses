c1101376da8be3a0ac533cdd2727548ddf12d961
Pistoled999 <yuri_matteus@hotmail.com>
2023-04-23 00:34:24 -0300

    primeira versão

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const ExpenseApp());
+
+class ExpenseApp extends StatelessWidget {
+  const ExpenseApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Despesas pessoais"),
+      ),
+      body: const Center(
+        child: Text("Versão inicial"),
+      ),
+    );
+  }
+}
